7deaf802ec9489896fdb2ce4e318cafd7c40c7a6
Gabriel Ziegler <gabrielziegler3@gmail.com>
2018-07-31 16:36:40 -0300

    Change colorscheme, add RAM, CPU, SSD, keyboard and network info to polybar

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 # Terminate already running bar instances
-#killall -q polybar
+killall -q polybar
 
 # Wait until the processes have been shut down
 #while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
@@ -12,8 +12,8 @@
 
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload example &
+    MONITOR=$m polybar --reload topbar &
   done
 else
-  polybar --reload example &
+  polybar --reload topbar &
 fi
