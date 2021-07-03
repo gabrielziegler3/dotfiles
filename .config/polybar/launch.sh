be2e1d3432576ea8b10fdf4eab9f14e74cfd1c54
Gabriel Ziegler <gabrielziegler3@gmail.com>
2021-07-03 19:09:31 -0300

    Update polybar config for 4K display
    Signed-off-by: Gabriel Ziegler <gabrielziegler3@gmail.com>

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,14 +6,16 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# if type "xrandr"; then
-#   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-#     MONITOR=$m polybar --reload topbar &
-#   done
-# else
-#   polybar --reload topbar &
-# fi
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    # MONITOR=$m polybar --reload topbar &
+    MONITOR=$m polybar -c ${HOME}/.config/polybar/config left &
+    MONITOR=$m polybar -c ${HOME}/.config/polybar/config center &
+    MONITOR=$m polybar -c ${HOME}/.config/polybar/config right &
+  done
+else
+    polybar -c ${HOME}/.config/polybar/config left &
+    polybar -c ${HOME}/.config/polybar/config center &
+    polybar -c ${HOME}/.config/polybar/config right &
+fi
 
-polybar -c ${HOME}/.config/polybar/config left &
-polybar -c ${HOME}/.config/polybar/config center &
-polybar -c ${HOME}/.config/polybar/config right &
