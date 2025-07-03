6b8d5f26e6c069a6b2a15cbeee644e0bb1d750b0
Gabriel Ziegler <gabrielziegler3@gmail.com>
2025-07-03 09:26:38 -0300

    chore: fix tab for autocomplete copilot

--- .config/nvim/lua/theprimeagen/lazy/copilot.lua
+++ .config/nvim/lua/theprimeagen/lazy/copilot.lua
@@ -1,10 +1,18 @@
 return {
-    "github/copilot.vim",
-    -- accept with tab
-    config = function()
-        vim.g.copilot_node_command = "node"
-        vim.g.copilot_no_tab_map = true
-        vim.g.copilot_assume_mapped = true
-        vim.g.copilot_enabled = true
-     end
-    }
+  "github/copilot.vim",
+  config = function()
+    vim.g.copilot_node_command = "node"
+    vim.g.copilot_no_tab_map = true
+    vim.g.copilot_assume_mapped = true
+    vim.g.copilot_enabled = true
+
+    -- Use Tab to accept Copilot suggestion
+    vim.api.nvim_set_keymap("i", "<Tab>", 'copilot#Accept("<Tab>")', {
+      expr = true,
+      silent = true,
+      noremap = true,
+      replace_keycodes = false
+    })
+  end
+}
+
